812fe383137f527ad86f91294355c6bbc1a531d2
andre1999lopes <andrejunior.cardoso@arkmeds.com>
2025-12-02 18:21:54 -0300

    Changed hard coded variable to env variable

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:3000', // ajuste para a URL do seu frontend
+    origin: process.env.FRONT_URL, // ajuste para a URL do seu frontend
     credentials: true
   })
 );
